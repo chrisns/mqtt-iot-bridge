921301d727edf51d3f7dd10332278942a4d4579c
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-10-10 22:18:44 +0100

    more logging

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const awsMqttClient = new AWSMqtt({
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
   endpointAddress: AWS_IOT_ENDPOINT_HOST,
   region: AWS_REGION
-});
+})
 
 const mqttClient = mqtt.connect(MQTT_HOST, {
   username: MQTT_USER,
@@ -16,19 +16,22 @@ const mqttClient = mqtt.connect(MQTT_HOST, {
 })
 
 awsMqttClient.on('connect', () => {
-  awsMqttClient.subscribe(AWS_SUBSCRIBE, {qos: 1});
-  console.log('AWS connected');
-});
+  awsMqttClient.subscribe(AWS_SUBSCRIBE, {qos: 1})
+  console.log('AWS connected')
+})
 
-awsMqttClient.on('message', (topic, message) => mqttClient.publish(topic, message, {qos: 2}));
+awsMqttClient.on('message', (topic, message) =>
+  mqttClient.publish(topic, message, {qos: 2}, err =>
+    console.log("aws", err, topic, message.toString())
+  ))
 
-awsMqttClient.on('error', (error) => console.error("aws - error", error));
+awsMqttClient.on('error', (error) => console.error("aws - error", error))
 
-awsMqttClient.on('close', (error) => console.error('aws - close', error));
+awsMqttClient.on('close', (error) => console.error('aws - close', error))
 
-awsMqttClient.on('offline', (error) => console.error('aws -  offline', error));
+awsMqttClient.on('offline', (error) => console.error('aws -  offline', error))
 
-awsMqttClient.on('reconnect', (error) => console.log('aws - reconnect', error));
+awsMqttClient.on('reconnect', (error) => console.log('aws - reconnect', error))
 
 mqttClient.on('connect', () => {
   mqttClient.subscribe(MQTT_SUBSCRIBE, {qos: 2})
@@ -41,5 +44,8 @@ mqttClient.on('close', () => console.error("mqtt - close"))
 
 mqttClient.on('offline', () => console.log("mqtt - offline"))
 
-mqttClient.on('message', (topic, message) => awsMqttClient.publish(topic, message, {qos: 1}));
+mqttClient.on('message', (topic, message) =>
+  awsMqttClient.publish(topic, message, {qos: 1}, err =>
+    console.log("aws", err, topic, message.toString())
+  ))
 
